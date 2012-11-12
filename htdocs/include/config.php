f769d3aec308eaca6322dc492f7541a4e56e652d
Jethro Carr <jethro.carr@jethrocarr.com>
2012-11-12 11:44:02 +0000

    * Fixed schema version

--- htdocs/include/config.php
+++ htdocs/include/config.php
@@ -24,10 +24,10 @@ $GLOBALS["config"] = array();
 
 // define the application details
 $GLOBALS["config"]["app_name"]			= "FlatTraffic";
-$GLOBALS["config"]["app_version"]		= "1.0.0";
+$GLOBALS["config"]["app_version"]		= "1.0.0_beta_1";
 
 // define the schema version required
-$GLOBALS["config"]["schema_version"]		= "20110604";
+$GLOBALS["config"]["schema_version"]		= "20121020";
 
 
 /*
